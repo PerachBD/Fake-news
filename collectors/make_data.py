99588f762f9fa22d43246f47fe1a2106334c03eb
FlowerBD <Perach13@gmail.com>
2017-08-29 11:54:56 +0300

    change the writing to json file

--- collectors/make_data.py
+++ collectors/make_data.py
@@ -1,5 +1,6 @@
 from newspaper import Article
-from json import dumps, load
+import json
+import os
 
 fake_or_real = raw_input("entr F for fake and R for real: ")
 url = raw_input('enter news url: ')
@@ -19,9 +20,19 @@ article.parse()
 strdate = str(article.publish_date)
 date = strdate.split(' ')
 
-with open("../data/my_data.txt", "a+") as datafile:
-    datafile.write(dumps({'source_url': article.source_url, 'date': date[0], 'title': article.title, 'authors': article.authors, 'images': article.images, 'text': article.text, 'fake or real': fake_or_real}, datafile, indent=7))
-datafile.close()
+# with open("../data/my_data.json", "a+") as datafile:
+#     datafile.write(dumps({'source_url': article.source_url, 'date': date[0], 'title': article.title, 'authors': article.authors, 'images': article.images, 'text': article.text, 'fake or real': fake_or_real}, datafile, indent=7))
+# datafile.close()
+#
 
-with open("../data/my_data.txt", "r") as s:
-    print(s.read())
+data_file_path = os.path.abspath("../data/my_data.json")
+data = []
+with open(data_file_path, 'r+') as datafile:
+    content = datafile.read()
+    content = [] if content == '' else content
+    data = json.loads(content)
+    data.append({'source_url': article.source_url, 'date': date[0], 'title': article.title, 'authors': article.authors,
+         'images': article.images, 'text': article.text, 'fake or real': fake_or_real})
+
+with open(data_file_path, 'w') as datafile:
+    datafile.write(json.dumps(data))
